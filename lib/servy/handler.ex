87e70ec25ef8675c949f17ea2b69833fb711a72c
baroque-works <ivanvazquezweb@gmail.com>
2026-05-10 03:39:31 -0600

    Conv struct instead generic maps. Adding support to query params in POST requests

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,11 +1,12 @@
 defmodule Servy.Handler do
-
   @moduledoc """
   Handles HTTP requests.
   """
 
+  alias Servy.Conv
+
   @pages_path Path.expand("../../pages", __DIR__)
-  
+
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
   import Servy.Parser, only: [parse: 1]
 
@@ -18,72 +19,69 @@ defmodule Servy.Handler do
     |> track
     |> format_response
   end
-  
-  def route(%{ method: "GET", path: "/wildthings" } = conv) do
-    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
+
+  def route(%Conv{method: "GET", path: "/wildthings"} = conv) do
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
-  
-  def route(%{ method: "GET", path: "/about" } = conv) do
-    @pages_path 
+
+  def route(%Conv{method: "GET", path: "/about"} = conv) do
+    @pages_path
     |> Path.join("about.html")
     |> File.read()
     |> handle_file(conv)
   end
-             
-  def route(%{ method: "GET", path: "/bears" } = conv) do
-    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
+
+  def route(%Conv{method: "GET", path: "/bears"} = conv) do
+    %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
-  def route(%{ method: "GET", path: "/bears/new" } = conv) do
+  def route(%Conv{method: "POST", path: "/bears"} = conv) do
+    %{
+      conv
+      | status: 201,
+        resp_body: "Created a #{conv.params["type"]} bear named #{conv.params["name"]}!"
+    }
+  end
+
+  def route(%Conv{method: "GET", path: "/bears/new"} = conv) do
     @pages_path
     |> Path.join("form.html")
     |> File.read()
     |> handle_file(conv)
   end
 
-  def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
-    %{ conv | status: 200, resp_body: "Bear #{id}" }
-  end
-  
-  def route(%{ method: "DELETE", path: "/bears/" <> _id } = conv) do 
-    %{ conv | status: 403, resp_body: "Deleting a bear is forbidden!" }
+  def route(%Conv{method: "GET", path: "/bears/" <> id} = conv) do
+    %{conv | status: 200, resp_body: "Bear #{id}"}
   end
 
-  def route(%{path: path} = conv) do
-    %{ conv | status: 404, resp_body: "No #{path} here!"}
+  def route(%Conv{method: "DELETE", path: "/bears/" <> _id} = conv) do
+    %{conv | status: 403, resp_body: "Deleting a bear is forbidden!"}
+  end
+
+  def route(%Conv{path: path} = conv) do
+    %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
   def handle_file({:ok, content}, conv) do
-    %{ conv | status: 200, resp_body: content }
+    %{conv | status: 200, resp_body: content}
   end
 
   def handle_file({:error, :enoent}, conv) do
-    %{ conv | status: 404, resp_body: "File not found!" }
-  end
-   
-  def handle_file({:error, reason}, conv) do
-    %{ conv | status: 500, resp_body: "File error: #{reason}" }
+    %{conv | status: 404, resp_body: "File not found!"}
   end
 
-  def format_response(conv) do
+  def handle_file({:error, reason}, conv) do
+    %{conv | status: 500, resp_body: "File error: #{reason}"}
+  end
+
+  def format_response(%Conv{} = conv) do
     """
-    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
+    HTTP/1.1 #{Conv.full_status(conv)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
     #{conv.resp_body}
     """
-  end
-
-  defp status_reason(code) do
-    %{
-      200 => "OK",
-      201 => "Created",
-      401 => "Unauthorized",
-      403 => "Forbidden",
-      404 => "Not Found",
-      500 => "Internal Server Error"
-    }[code]
   end
 end
 
@@ -96,8 +94,7 @@ Accept: */*
 """
 
 response = Servy.Handler.handle(request)
-IO.puts response
-
+IO.puts(response)
 
 request = """
 GET /bears/new HTTP/1.1
@@ -108,10 +105,9 @@ Accept: */*
 """
 
 response = Servy.Handler.handle(request)
-IO.puts response
+IO.puts(response)
 
-
-equest = """
+request = """
 GET /wildthings HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
@@ -120,8 +116,7 @@ Accept: */*
 """
 
 response = Servy.Handler.handle(request)
-IO.puts response
-
+IO.puts(response)
 
 request = """
 GET /wildlife HTTP/1.1
@@ -132,8 +127,7 @@ Accept: */*
 """
 
 response = Servy.Handler.handle(request)
-IO.puts response
-
+IO.puts(response)
 
 request = """
 GET /bears HTTP/1.1
@@ -142,9 +136,9 @@ User-Agent: ExampleBrowser/1.0
 Accept: */*
 
 """
-response = Servy.Handler.handle(request)
-IO.puts response
 
+response = Servy.Handler.handle(request)
+IO.puts(response)
 
 request = """
 GET /bigfoot HTTP/1.1
@@ -153,9 +147,9 @@ User-Agent: ExampleBrowser/1.0
 Accept: */*
 
 """
-response = Servy.Handler.handle(request)
-IO.puts response
 
+response = Servy.Handler.handle(request)
+IO.puts(response)
 
 request = """
 GET /bears/1 HTTP/1.1
@@ -164,9 +158,9 @@ User-Agent: ExampleBrowser/1.0
 Accept: */*
 
 """
-response = Servy.Handler.handle(request)
-IO.puts response
 
+response = Servy.Handler.handle(request)
+IO.puts(response)
 
 request = """
 DELETE /bears/1 HTTP/1.1
@@ -175,7 +169,21 @@ User-Agent: ExampleBrowser/1.0
 Accept: */*
 
 """
+
 response = Servy.Handler.handle(request)
-IO.puts response
+IO.puts(response)
 
+request = """
+POST /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 21
 
+name=Baloo&type=Brown
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
